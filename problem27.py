21e29057141e416c6f4b5858a2a07487237514f8
Tasnim <tasnimdrmc6461@gmail.com>
2026-04-23 23:22:21 +0600

    problem 27 solved

---
+++ problem27.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        i = 0
+        n = len(nums)
+
+        while i < n-1:
+            if nums[i] == val:
+                nums[i] = nums[n-1]
+                n -= 1
+            else:
+                i+=1
+
+        return n
